e56ed0fe417dcabf1f03b87e073e8a4ce7954a2d
Ramiro Brescia <ramiro.brescia@nano-apps.com>
2025-03-13 00:57:19 -0300

    Add ProductPrice model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
@@ -13,5 +14,21 @@ class Product extends Model
     public function provider(): HasOne
     {
         return $this->hasOne(Provider::class);
+    }
+
+    /**
+     * Get the lastest price.
+     */
+    public function price(): HasOne
+    {
+        return $this->hasOne(ProductPrice::class)->latestOfMany();
+    }
+
+    /**
+     * Get the price history.
+     */
+    public function prices(): HasMany
+    {
+        return $this->hasMany(ProductPrice::class);
     }
 }
