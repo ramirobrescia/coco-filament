dbf99ad3c752867955c4cda36f12dd808da34227
Ramiro Brescia <ramirobrescia@gmail.com>
2025-05-06 23:55:01 -0300

    Add the built in filament user registration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->colors([
                 'amber' => Color::Amber,
                 'lime' => Color::Lime,
